aa7e8a1bd85d95b6fd567fae0af0a98f63969d8f
mustafa <mustafa.sadriwala@utdallas.edu>
2019-12-14 16:06:47 -0600

    Updates

---
+++ precache-manifest.d22af422847df4b69acce4583919961c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c02c646624682b4d24f29330ab4d06",
+    "url": "/murtuza-art/index.html"
+  },
+  {
+    "revision": "b826a69990cc50f6c4a8",
+    "url": "/murtuza-art/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7b57ac0c70be8cbeebce",
+    "url": "/murtuza-art/static/js/2.1e49a6c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/murtuza-art/static/js/2.1e49a6c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b826a69990cc50f6c4a8",
+    "url": "/murtuza-art/static/js/main.b8491710.chunk.js"
+  },
+  {
+    "revision": "e57e56e8b7f58352b40f",
+    "url": "/murtuza-art/static/js/runtime-main.a1e205da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/murtuza-art/static/media/logo.5d5d9eef.svg"
+  }
+]);
